02145239097a3029f0c885ecb791cc17835ed88e
hudson martins <hudsonlh.martins@gmail.com>
2024-03-25 22:21:22 -0300

    feat: add fastify/jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { registerController } from './controllers/register-controller'
-import { autheticateController } from './controllers/authenticate-controller'
+import { registerController } from './controllers/register.controller'
+import { autheticateController } from './controllers/authenticate.controller'
+import { profile } from './controllers/profile.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', autheticateController)
+
+  /* Authenticated */
+  app.get('/me', profile)
 }
