e169f32964f6cfd266786e3a27652d977118b3fd
hudson martins <hudsonlh.martins@gmail.com>
2024-03-26 20:01:32 -0300

    feat: add midd to auth router

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,8 +1,14 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request:FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
 
-  console.log(request.user.sub)
-  return reply.status(200).send({ message: "Profile" });
+  const userId = request.user.sub
+
+  const getUseProfile =  makeGetUserProfileUseCase()
+  const {user} = await getUseProfile.execute({
+    userId
+  })
+  const {password_hash, ...userWithoutPassword} = user
+  return reply.status(200).send(userWithoutPassword);
 }
